7db9610aea171651a5805b67fcc3106423d64c8f
chengzeyi <ichengzeyi@gmail.com>
2023-12-27 12:30:22 +0800

    fix

--- ubuntu-desktop/docker_config/post_install.sh
+++ ubuntu-desktop/docker_config/post_install.sh
@@ -1,5 +1,7 @@
 set -e
 
+sudo apt autoremove -y && sudo apt clean
+
 # Install Vscode.
 sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -12,6 +14,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
+sudo apt autoremove -y && sudo apt clean
 
 # sudo apt-get install -y python3-dev python3-pip
 
@@ -25,6 +28,7 @@ sudo update-alternatives --set editor /usr/bin/nvim
 # Install NodeJS
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
+sudo apt autoremove -y && sudo apt clean
 
 # Install CFW
 curl https://archive.org/download/clash_for_windows_pkg/Clash.for.Windows-0.20.39-x64-linux.tar.gz | tar -xz
@@ -33,8 +37,6 @@ mv Clash\ for\ Windows-*-x64-linux/ cfw
 # sudo rm /usr/local/bin/cfw
 sudo mv cfw /etc/cfw
 sudo ln -s /etc/cfw/cfw /usr/local/bin/cfw
-
-sudo apt autoremove -y
 
 # Use apt mirror
 UBUNTU_DISTRO=$(lsb_release -cs)
